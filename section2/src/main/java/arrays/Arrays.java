18166b28570628ce33fbd0d5d62a6c3d343e7efc
TajBanana <ytahjian@gmail.com>
2022-02-04 03:04:00 +0800

    javads section2

---
+++ section2/src/main/java/arrays/Arrays.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+
+//        every int occupies 4 bytes in memory
+//        when we are working with object array, what is stored in memory is the object reference, and they are of the same size
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = -55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for (int i : intArray) {
+            System.out.println(i);
+        }
+    }
+}
