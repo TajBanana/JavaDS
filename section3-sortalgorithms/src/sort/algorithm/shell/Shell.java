938d3128986001d28c6361fdc47768df7e0d4541
TajBanana <ytahjian@gmail.com>
2022-02-18 16:07:50 +0800

    shell sort start

--- section3-sortalgorithms/src/sort/algorithm/shell/Shell.java
+++ section3-sortalgorithms/src/sort/algorithm/shell/Shell.java
@@ -1,8 +1,8 @@
-package sort.algorithm;
+package sort.algorithm.shell;
 
-public class Main {
-
+public class Shell {
     public static void main(String[] args) {
         int [] intArray = { 20, 35, -15, 7, 55, 1, -22};
+
     }
 }
