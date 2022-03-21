c39bc7f322926a87158f17331382b1a2ab5f6bcf
TajBanana <ytahjian@gmail.com>
2022-03-21 23:48:30 +0800

    stack

---
+++ section4-lists/src/main/java/stack/jdk/Employee.java
@@ -0,0 +1,14 @@
+package stack.jdk;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+}
