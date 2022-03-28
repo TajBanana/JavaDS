fab49793033b8d5b6b85e8c509f817e822fcf6bb
TajBanana <ytahjian@gmail.com>
2022-03-28 09:25:37 +0800

    section 5 stack

--- section4-lists/src/main/java/stack/jdk/Main.java
+++ section4-lists/src/main/java/stack/jdk/Main.java
@@ -8,6 +8,8 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
+
+
         Employee janeJones = new Employee("Jane", "Jones", 123);
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
